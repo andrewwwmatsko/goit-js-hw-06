d85c5f218be652b6154ad110f667ac52d17e5805
andrewwwmatsko <matskoandrew@gmail.com>
2024-03-31 23:37:20 +0300

    task-2

---
+++ js/task-2.js
@@ -0,0 +1,30 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    for (const item of this.#items) {
+      if (item === itemToRemove) {
+        this.#items.splice(this.#items.indexOf(item), 1);
+      }
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
